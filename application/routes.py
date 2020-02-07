cdc61b9dff8c1cc4c0335f48f9fdf7630ca446ab
weilyuwang <weilyuwang@gmail.com>
2020-02-06 21:14:15 -0500

    add login, register, courses templates

--- application/routes.py
+++ application/routes.py
@@ -4,8 +4,9 @@ from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html")
 
 
 @app.route("/login")
@@ -15,7 +16,15 @@ def login():
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html")
+    courseData = [
+        {"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
+        {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
+        {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, 
+        {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, 
+        {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}
+    ]
+
+    return render_template("courses.html", courseData=courseData)
 
 
 @app.route("/register")
