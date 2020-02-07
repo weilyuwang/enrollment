0fafe531551edc7f4f154d1b0c78f63e72161935
weilyuwang <weilyuwang@gmail.com>
2020-02-06 21:56:40 -0500

    add GET handler for /enrollment

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route("/")
@@ -14,8 +14,9 @@ def login():
     return render_template("login.html", login=True)
 
 
-@app.route("/courses")
-def courses():
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term="Spring 2019"):
     courseData = [
         {"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
         {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
@@ -24,9 +25,17 @@ def courses():
         {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}
     ]
 
-    return render_template("courses.html", courseData=courseData, courses=True)
+    return render_template("courses.html", courseData=courseData, courses=True, term=term)
 
 
 @app.route("/register")
 def register():
     return render_template("register.html", register=True)
+
+
+@app.route("/enrollment")
+def enrollment():
+    id = request.args.get('courseID')
+    title = request.args.get('title')
+    term = request.args.get('term')
+    return render_template("enrollment.html", enrollment=True, data={"id": id, "title": title, "term": term})
