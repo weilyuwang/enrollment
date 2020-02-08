50d8ce1cb421365e61d9c70397bbb1a8c6514cae
weilyuwang <weilyuwang@gmail.com>
2020-02-07 23:23:51 -0500

    fix pip libs in venv

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,6 @@ from flask_mongoengine import MongoEngine
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = MongoEngine()
-db.init_app(app)
+db = MongoEngine(app)
 
 from application import routes
