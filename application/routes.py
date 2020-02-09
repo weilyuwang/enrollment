266629fe232c26cdd6ab72ad9800e04b743d9ab4
weilyuwang <weilyuwang@gmail.com>
2020-02-09 13:40:26 -0500

    update login validation to interact with database

--- application/routes.py
+++ application/routes.py
@@ -22,9 +22,12 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print("form.validate_on_submit is TRUE!")
-        if request.form.get("email") == "test@uta.com":
-            flash("You are successfully logged in!", "success")
+        email    = form.email.data
+        password = form.password.data
+
+        user = User.objects(email=email).first()
+        if user and user.get_password(password):
+            flash(f"{user.first_name}, You are successfully logged in!", "success")
             return redirect("/index")
         else:
             flash("Sorry, something went wrong.", "danger")
