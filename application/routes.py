51fe6eaf80254f00684d8e82f6d490afc3d52b91
weilyuwang <weilyuwang@gmail.com>
2020-02-09 15:01:16 -0500

    refactor to pull course data from database

--- application/routes.py
+++ application/routes.py
@@ -3,13 +3,13 @@ from flask import render_template, request, json, Response, redirect, flash, url
 from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 
-courseData = [
-    {"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
-    {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
-    {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, 
-    {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, 
-    {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}
-]
+# courseData = [
+#     {"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, 
+#     {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, 
+#     {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, 
+#     {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, 
+#     {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}
+# ]
 
 @app.route("/")
 @app.route("/index")
@@ -36,9 +36,13 @@ def login():
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
-def courses(term="Spring 2019"):
-    return render_template("courses.html", courseData=courseData, courses=True, term=term)
+def courses(term=None):
+    if term is None:
+        term = "Spring 2019"
 
+    classes = Course.objects.order_by("+course_id")
+
+    return render_template("courses.html", courseData=classes, courses=True, term=term)
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
